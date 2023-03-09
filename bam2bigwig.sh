5f5f583ef26284e9c82d5a026e11483a23bbcd3f
aelzen2 <anouk.vandenelzen2@ru.nl>
2023-03-09 21:28:46 +0100

    add bam2bigwig

---
+++ bam2bigwig.sh
@@ -0,0 +1,26 @@
+
+
+input_dir=/scratch/bash_course/lecture_4/bams
+
+input_files=$(ls ${input_dir}/*.bam)
+
+output_dir=~/scratch/anouk
+
+mamba create --name bam2bigwig
+source $(~ $(~ $(which mamba)))/etc/profile.d/conda.sh
+conda activate bam2bigwig
+mamba install samtools
+
+for f in ${input_files[@]};do
+samtools index $f $f".bai"
+done"
+
+input_files_indexed=$(ls ${input_files}/*.bai)
+
+for i in ${input_files_indexed[@]};do
+
+file_without_suffix=${i::-4}
+bamCoverage -b $file_without_suffix -o $file_without_suffix".bw";
+done
+ 
+echo "Anouk"
